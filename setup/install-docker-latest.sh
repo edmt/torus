97cf7e7b8e5ac34b1e48ccd492cef1e1f5232835
Daniel Martinez <eduardodaniel2@gmail.com>
2014-03-04 11:53:52 -0600

    Añade un Vagrantfile basado en Ubuntu Precise 64

---
+++ setup/install-docker-latest.sh
@@ -0,0 +1,2 @@
+sudo apt-get install -y curl
+curl -s https://get.docker.io/ubuntu/ | sudo sh
---
+++ setup/install-linux-kernel-3.8.sh
@@ -0,0 +1,6 @@
+# install the backported kernel
+sudo apt-get update
+sudo apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
+
+# reboot
+sudo shutdown -h 0
